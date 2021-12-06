031ec658eb9d0727fb2dcb3ea9883aab96395b0f
JonatanR <jonatanjr-2017@outlook.es>
2021-12-06 07:59:19 -0500

    Subprograma Convertidor de Radianes a grados

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
-public class Main {
+import java.util.*;
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+
+
+
+    }
+    public static int ConvertirdorDeRadianes(double numero )
+    {
+        Scanner input = new Scanner(System.in);
+        int opcion;
+        int grado = ConvertirdorDeRadianes(4.3993);
+
+
+        System.out.print("¿es fraccion o numero decimal ? , Escribe '1'(fraccion) ó '2'(Entero o decimal) : ");
+        opcion = input.nextInt();
+        if(opcion ==1 )
+        {
+            double numerador=0, denominador=0 ;
+            double division=0;
+
+            System.out.println("Escribe primero el numerador (ENTER), despues ingresa el denominador por ultimo (ENTER)");
+
+            System.out.print("Resultado : "+(numerador = input.nextInt())+"/"+(denominador= input.nextInt()));
+            division = (double) (numerador / denominador);
+
+            System.out.print(" = "+((int)(division*Math.PI))+"°");
+
+        }else
+        {
+            int numero=0;
+            System.out.print("Ingresar Numero : ");
+            numero = input.nextInt();
+            System.out.println("Resultado : "+(((int)(numero*Math.PI))/180)+" π rad");
+
     }
 }
