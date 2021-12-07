de5f130c8aef60d4e850e91f0a238d34dc75cd85
JonatanR <jonatanjr-2017@outlook.es>
2021-12-06 19:34:12 -0500

    Punto(2), Bien

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,26 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args)
     {
-	// write your code here
+        Scanner lector = new Scanner(System.in);
+
+        int n, result=0, a,i;
+
+        System.out.println("Digite el numero a elevar al cubo: ");
+        n = lector.nextInt();
+
+        int aux = n*(n-1)/2 + 1;
+
+        for (i=0; i<n; i++)
+        {
+            a = (2*aux-1);
+            result = result+a;
+            ++aux;
+        }
+
+        System.out.println(n+" al cubo es : "+result);
+
     }
 }
