9e24fabf9483715726d15989cc7c8df3bf937a62
JonatanR <jonatanjr-2017@outlook.es>
2021-12-06 19:04:07 -0500

    Punto(1), En revision...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
+import java.util.*;
+public class Main
+{
 
-public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println();
+        Scanner lector = new Scanner (System.in);
 
-    public static void main(String[] args) {
-	// write your code here
+        int a,n, i, result = 0;
+
+        System.out.print("Digite el numero a elevar al cuadrado: ");
+        n = lector.nextInt();
+
+        for (i=1; i<=n; i++)
+        {
+            a = (2*i-1);
+            result = result+a;
+        }
+
+        System.out.println(n+" al cuadrado es : "+result);
     }
 }
