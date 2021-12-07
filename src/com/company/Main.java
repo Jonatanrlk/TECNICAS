22c39e94a783c6a5d2b565df1e502cd5588ccb64
JonatanR <jonatanjr-2017@outlook.es>
2021-12-07 13:04:08 -0500

    Punto 3 , Resuelto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,39 @@ package com.company;
 import java.util.*;
 public class Main
 {
-
     public static void main(String[] args)
     {
+        double sen=0;
+        double x;
+        int numero;
+        Scanner lector = new Scanner(System.in);
+
+        System.out.print("Ingrese el angulo en radianes: ");
+        x = lector.nextDouble();
+
+        System.out.print("Ingrese el numero de terminos: ");
+        numero = lector.nextInt();
+
+        int i=1;
+        while(i<=numero)
+        {
+            int fact = 1;
+            int j=1;
+            while(j<=(1+(2*(i-1))))
+            {
+                fact= fact*j;
+                j++;
+            }
+            if(i%2==0)
+                sen = sen - (Math.pow(x,(1+(2*(i-1)))) /fact);
+            else
+                sen = sen + (Math.pow(x,(1+(2*(i-1)))) /fact);
+            i++;
+        }
+        System.out.println("El seno de: " +x+ " es " +sen);
 
     }
+
     public static int ConvertirdorDeRadianes(double numero )
     {
         Scanner input = new Scanner(System.in);
